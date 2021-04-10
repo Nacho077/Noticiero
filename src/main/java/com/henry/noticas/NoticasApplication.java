6fbf16cc36868bfef8ad5e0bf67dd60839cdf6d1
Ignacio Gimenez <ignaciogimenez70@gmail.com>
2021-04-10 15:34:31 -0300

    first commit

---
+++ src/main/java/com/henry/noticas/NoticasApplication.java
@@ -0,0 +1,13 @@
+package com.henry.noticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoticasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henry/noticas/NoticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.noticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
